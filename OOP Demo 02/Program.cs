7449b56ecac82f28f9a0bf5afeec40591870b818
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2024-12-31 15:32:13 +0200

    Struct

---
+++ OOP Demo 02/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OOP_Demo_02
+{
+    internal class Program
+    {
+        //Entry Point
+        static void Main(string[] args)
+        {
+            #region Struct
+            //    Point P1;
+            //    //Declare for Object of type "Point
+            //    //// CLR Will Allocate 8 UnInitialized Bytes in Stack.
+
+            //    P1 = new Point(1 , 2);
+            //    // NEW --> IS JUST FOR THE CONSTRUCTOR selection that will initialize the struct object
+
+
+            //    Console.WriteLine(P1); // ( 1 , 2 )  // Most use but making Boxing 
+
+
+            //    Console.WriteLine(P1.ToString());  // x= 1 , y = 2
+            //}
+            #endregion
+        }
+    }
+}
