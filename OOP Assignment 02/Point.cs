d2534da9be80ce5832fa8260e8553c86c8ea49fd
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-01 17:41:20 +0200

    Part 01 Q 02

---
+++ OOP Assignment 02/Point.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment_02
+{
+    internal class Point
+    {
+
+    //    public double X, Y;
+
+    //    public Point(double x, double y)
+    //    {
+    //        X = x;
+    //        Y = y;
+    //    }
+    
+}
+}
--- OOP Assignment 02/Program.cs
+++ OOP Assignment 02/Program.cs
@@ -14,7 +14,7 @@
             //persons[1] = new Person("Ahmed", 30);
             //persons[2] = new Person("Khaled", 35);
 
-  
+
             //Console.WriteLine("Details of Persons:");
 
 
@@ -25,9 +25,37 @@
 
             #endregion
 
+            #region 2.	Create a struct called "Point" to represent a 2D point with properties "X" and "Y".
+            //Write a C# program that takes two points as input from the user and calculates the distance between them.
 
 
-            #endregion
-        }
+        //    Console.WriteLine("Enter the coordinates of the first point (X Y):");
+        //    Point p1 = ReadPoint();
+
+        //    Console.WriteLine("Enter the coordinates of the second point (X Y):");
+        //    Point p2 = ReadPoint();
+
+        //    double distance = CalculateDistance(p1, p2);
+        //    Console.WriteLine($"The distance between the points is: {distance}");
+        
+
+        //static Point ReadPoint()
+        //{
+        //    string[] input = Console.ReadLine().Split();
+        //    return new Point(double.Parse(input[0]), double.Parse(input[1]));
+        //}
+
+        //static double CalculateDistance(Point p1, Point p2)
+        //{
+        //    return Math.Sqrt((p2.X - p1.X) * (p2.X - p1.X) + (p2.Y - p1.Y) * (p2.Y - p1.Y));
+        //}
+
+
+        #endregion
+
+
+
+        #endregion
+    }
     }
 }
