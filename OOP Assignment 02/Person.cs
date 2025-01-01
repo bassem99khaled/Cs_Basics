924e53f58c7327f3d6b928a603f214051aa6c00d
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-01 17:26:38 +0200

    Part 01 Q 01

---
+++ OOP Assignment 02/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment_02
+{
+    internal class Person
+    { 
+      
+            //public string Name;
+            //public int Age;
+
+            //public Person(string name, int age)
+            //{
+            //    Name = name;
+            //    Age = age;
+            //}
+       
+
+      
+      
+
+
+
+}
+}
---
+++ OOP Assignment 02/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace OOP_Assignment_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Part 01
+            #region 1.	Define a struct "Person" with properties "Name" and "Age".
+            //Create an array of three "Person" objects and populate it with data. Then, write a C# program to display the details of all the persons in the array.
+
+
+            //Person[] persons = new Person[3];
+            //persons[0] = new Person("Bassem", 25);
+            //persons[1] = new Person("Ahmed", 30);
+            //persons[2] = new Person("Khaled", 35);
+
+  
+            //Console.WriteLine("Details of Persons:");
+
+
+            //foreach (var person in persons)
+            //{
+            //    Console.WriteLine($"Name: {person.Name}, Age: {person.Age}");
+            //}
+
+            #endregion
+
+
+
+            #endregion
+        }
+    }
+}
