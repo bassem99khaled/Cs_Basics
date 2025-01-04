070a43d88822b22f6f1462d1ddc558063826c286
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-04 18:14:29 +0200

    OverLoading

---
+++ Demo 03/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace Demo_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region OOP Pillars 1. PolyMorphism
+
+            #region OverLoading
+
+            // static int Sum02Numbers(int X , int Y)
+            // {
+            //     return X + Y;
+            // }
+
+            // static int sum ( int X , int Y , int Z)
+            // {
+            //     return X +Y + Z;
+            // }
+
+            // static double Sum( double X , double Y )
+            // {
+            //     return X * Y;
+            // }
+
+            //static double Sum( int x , double y)
+            // {
+            //     return x + y;
+            // }
+            // dynamic Obj = "Ahmed";  // Dynamic like Var in java script
+            //                             // dynamic make load in run time not use every time
+            // Obj = 10;
+
+            // Obj = true;
+
+            // dynamic Result = Sum02Numbers ( 1 , 2 );
+
+            // Console.WriteLine( $" Result {Result}" );
+
+            // Console.WriteLine("Ahmed");
+            // Console.WriteLine("A");
+            // Console.WriteLine(10);
+            // Console.WriteLine(true);
+
+            #endregion
+
+           
+
+            #endregion
+
+
+        }
+    }
+}
