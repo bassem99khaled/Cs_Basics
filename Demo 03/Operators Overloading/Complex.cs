52cdf5332fbb597c5780d77f98b6412bf1e71155
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-04 18:39:25 +0200

    Binary Operators

---
+++ Demo 03/Operators Overloading/Complex.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_03.Operators_Overloading
+{
+    internal class Complex
+    {
+
+        public int Real { get ; set; }
+
+        public int Imag { get; set; }
+
+
+
+        #region Opperators OverLoading
+        // OverLoading Operators ; must be Non-Private Class Member Function
+
+        #region Binary Operators
+        public static Complex operator + (Complex left , Complex Right)
+        {
+
+            return new Complex()
+            {
+                Real = (left?.Real ?? 0) + (Right?.Real ?? 0) ,
+                Imag = (left?.Imag ?? 0) + (Right?.Imag ?? 0),
+            };
+            // left?.Real
+            // Left != Null ? left.Real : null
+        }
+
+        public static Complex operator - (Complex left, Complex Right)
+        {
+
+            return new Complex()
+            {
+                Real = (left?.Real ?? 0) - (Right?.Real ?? 0),
+                Imag = (left?.Imag ?? 0) - (Right?.Imag ?? 0),
+            };
+
+        #endregion
+        public override string ToString()
+        {
+            return $"{Real} + {Imag}";
+        }
+    }
+}
--- Demo 03/Program.cs
+++ Demo 03/Program.cs
@@ -1,55 +1,77 @@
-﻿namespace Demo_03
+﻿using System.Numerics;
+namespace Demo_03.Operators_Overloading
 {
-    internal class Program
+
+    namespace Demo_03
     {
-        static void Main(string[] args)
+        internal class Program
         {
+            static void Main(string[] args)
+            {
 
-            #region OOP Pillars 1. PolyMorphism
+                #region OOP Pillars 1. PolyMorphism
 
-            #region OverLoading
+                #region OverLoading
 
-            // static int Sum02Numbers(int X , int Y)
-            // {
-            //     return X + Y;
-            // }
+                // static int Sum02Numbers(int X , int Y)
+                // {
+                //     return X + Y;
+                // }
 
-            // static int sum ( int X , int Y , int Z)
-            // {
-            //     return X +Y + Z;
-            // }
+                // static int sum ( int X , int Y , int Z)
+                // {
+                //     return X +Y + Z;
+                // }
 
-            // static double Sum( double X , double Y )
-            // {
-            //     return X * Y;
-            // }
+                // static double Sum( double X , double Y )
+                // {
+                //     return X * Y;
+                // }
 
-            //static double Sum( int x , double y)
-            // {
-            //     return x + y;
-            // }
-            // dynamic Obj = "Ahmed";  // Dynamic like Var in java script
-            //                             // dynamic make load in run time not use every time
-            // Obj = 10;
+                //static double Sum( int x , double y)
+                // {
+                //     return x + y;
+                // }
+                // dynamic Obj = "Ahmed";  // Dynamic like Var in java script
+                //                             // dynamic make load in run time not use every time
+                // Obj = 10;
 
-            // Obj = true;
+                // Obj = true;
 
-            // dynamic Result = Sum02Numbers ( 1 , 2 );
+                // dynamic Result = Sum02Numbers ( 1 , 2 );
 
-            // Console.WriteLine( $" Result {Result}" );
+                // Console.WriteLine( $" Result {Result}" );
 
-            // Console.WriteLine("Ahmed");
-            // Console.WriteLine("A");
-            // Console.WriteLine(10);
-            // Console.WriteLine(true);
+                // Console.WriteLine("Ahmed");
+                // Console.WriteLine("A");
+                // Console.WriteLine(10);
+                // Console.WriteLine(true);
 
-            #endregion
+                #endregion
 
-           
+                #region Binary Operators 
 
-            #endregion
+                //Complex C1 = new Complex() { Real = 2, Imag = 4 };
+                //Complex C2 = new Complex() { Real = 3, Imag = 5 };
+
+                //Complex C3 = default; // Null
 
 
+            
+                //C3 = C1 + C2;
+
+                //Complex C4 = C1 + C2 + C3;
+
+                //Console.WriteLine($"C1 = {C1}");
+                //Console.WriteLine($"C2 = {C2}");
+                //Console.WriteLine("______________________");
+                //Console.WriteLine($" C3 = {C3}");
+                #endregion
+
+                #endregion
+
+
+            }
         }
     }
 }
