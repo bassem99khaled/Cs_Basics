4c13e78d8eb784f656206500dc704c1b6c8391fd
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-05 20:56:34 +0200

    Part 01 Q 01

---
+++ OOP Assginment 03/Calculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assginment_03
+{
+    internal class Calculator
+    {
+
+
+        //public int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+
+
+        //public int Add(int a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+
+
+        //public double Add(double a, double b)
+        //{
+        //    return a + b;
+        //}
+    }
+}
---
+++ OOP Assginment 03/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace OOP_Assginment_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-Write a class named Calculator that contains a method named Add. Overload the Add method to:
+            //•	Accept two integers and return their sum.
+            //•	Accept three integers and return their sum.
+            //•	Accept two doubles and return their sum.
+            
+  
+                //Calculator calc = new Calculator();
+
+                //// Test overloaded methods
+                //Console.WriteLine("Sum of 2 and 3 (int): " + calc.Add(2, 3)); // Calls Add(int, int)
+                //Console.WriteLine("Sum of 1, 2, and 3 (int): " + calc.Add(1, 2, 3)); // Calls Add(int, int, int)
+                //Console.WriteLine("Sum of 2.5 and 3.5 (double): " + calc.Add(2.5, 3.5)); // Calls Add(double, double)
+            
+        
+    
+
+
+    #endregion
+
+
+    #region
+
+    #endregion
+
+
+    #region
+
+
+
+    #endregion
+
+    #region
+
+
+    #endregion
+
+
+
+    #region
+
+
+
+    #endregion
+
+
+}
+    }
+}
