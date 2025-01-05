8bf6827edb32bc98d4c67475e4c93249fe67fba7
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-05 21:36:34 +0200

    Part 01 Q 02

--- OOP Assginment 03/Program.cs
+++ OOP Assginment 03/Program.cs
@@ -10,23 +10,44 @@ namespace OOP_Assginment_03
             //•	Accept two integers and return their sum.
             //•	Accept three integers and return their sum.
             //•	Accept two doubles and return their sum.
-            
-  
-                //Calculator calc = new Calculator();
-
-                //// Test overloaded methods
-                //Console.WriteLine("Sum of 2 and 3 (int): " + calc.Add(2, 3)); // Calls Add(int, int)
-                //Console.WriteLine("Sum of 1, 2, and 3 (int): " + calc.Add(1, 2, 3)); // Calls Add(int, int, int)
-                //Console.WriteLine("Sum of 2.5 and 3.5 (double): " + calc.Add(2.5, 3.5)); // Calls Add(double, double)
-            
-        
-    
 
 
-    #endregion
+            //Calculator calc = new Calculator();
+
+            //// Test overloaded methods
+            //Console.WriteLine("Sum of 2 and 3 (int): " + calc.Add(2, 3)); // Calls Add(int, int)
+            //Console.WriteLine("Sum of 1, 2, and 3 (int): " + calc.Add(1, 2, 3)); // Calls Add(int, int, int)
+            //Console.WriteLine("Sum of 2.5 and 3.5 (double): " + calc.Add(2.5, 3.5)); // Calls Add(double, double)
 
 
-    #region
+
+
+
+            #endregion
+
+
+            #region 2-Create a class named Rectangle with the following constructors:
+            //	A parameterless constructor that sets the width and height to 0.
+            //	A constructor that accepts width and height as integers.
+            //	A constructor that accepts a single integer and sets both width and height to that value.
+
+   
+           
+
+                //Rectangle rect1 = new Rectangle(); 
+                //Rectangle rect2 = new Rectangle(10, 20); 
+                //Rectangle rect3 = new Rectangle(15); 
+
+                
+                //Console.WriteLine("Rectangle 1:");
+                //rect1.DisplayDimensions();
+
+                //Console.WriteLine("\nRectangle 2:");
+                //rect2.DisplayDimensions();
+
+                //Console.WriteLine("\nRectangle 3:");
+                //rect3.DisplayDimensions();
+     
 
     #endregion
 
---
+++ OOP Assginment 03/Rectangle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assginment_03
+{
+    internal class Rectangle
+    {
+
+        #region paremeter
+        //public int Width { get; set; }
+        //public int Height { get; set; }
+
+        #endregion
+
+        #region Constructor
+        //public Rectangle()
+        //{
+        //    Width = 0;
+        //    Height = 0;
+        //}
+
+       
+        //public Rectangle(int width, int height)
+        //{
+        //    Width = width;
+        //    Height = height;
+        //}
+
+
+        //public Rectangle(int size)
+        //{
+        //    Width = size;
+        //    Height = size;
+        //}
+        #endregion
+
+        #region Method
+
+        //public void DisplayDimensions()
+        //{
+        //    Console.WriteLine($"Width: {Width}, Height: {Height}");
+        //}
+
+        #endregion
+
+    }
+
+}
