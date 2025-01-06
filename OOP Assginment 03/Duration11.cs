8f29d30348a9ceb5747ffb14e9f8dbef05078fa5
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-06 03:14:24 +0200

    Part 02 Q01

---
+++ OOP Assginment 03/Duration11.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assginment_03
+{
+    internal class Duration11
+    {
+        
+            #region Properties
+            public int Hours { get; set; }
+            public int Minutes { get; set; }
+            public int Seconds { get; set; }
+
+#endregion
+
+            #region Constructor
+            public Duration11(int hours, int minutes, int seconds)
+            {
+    
+                if (seconds >= 60)
+                {
+                    minutes += seconds / 60;
+                    seconds %= 60;
+                }
+
+                if (minutes >= 60)
+                {
+                    hours += minutes / 60;
+                    minutes %= 60;
+                }
+
+                Hours = hours;
+                Minutes = minutes;
+                Seconds = seconds;
+            }
+
+            public Duration11()
+            {
+                Hours = 0;
+                Minutes = 0;
+                Seconds = 0;
+            }
+
+#endregion
+
+            #region Method
+            public void DisplayDuration()
+            {
+                Console.WriteLine($"Duration: {Hours:D2}:{Minutes:D2}:{Seconds:D2}");
+            }
+            #endregion
+
+           
+
+}
+}
