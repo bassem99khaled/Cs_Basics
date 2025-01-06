153712a7c924b4d480d233a9305696c1b064f343
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-06 02:55:23 +0200

    Part 02 Q 03

--- OOP Assginment 03/Duration.cs
+++ OOP Assginment 03/Duration.cs
@@ -16,27 +16,18 @@ namespace OOP_Assginment_03
         #endregion
 
         #region Constructors
-      
         public Duration(int hours, int minutes, int seconds)
         {
-          
-            if (seconds >= 60)
-            {
-                minutes += seconds / 60;
-                seconds %= 60;
-            }
-
-            if (minutes >= 60)
-            {
-                hours += minutes / 60;
-                minutes %= 60;
-            }
-
-            Hours = hours;
-            Minutes = minutes;
-            Seconds = seconds;
+            NormalizeDuration(hours, minutes, seconds);
         }
 
+        
+        public Duration(int totalSeconds)
+        {
+            NormalizeDuration(0, 0, totalSeconds);
+        }
+
+      
         public Duration()
         {
             Hours = 0;
@@ -44,48 +35,34 @@ namespace OOP_Assginment_03
             Seconds = 0;
         }
 
-        public override string ToString()
-        {
-            return $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
-        }
-
-        public override bool Equals(object obj)
-        {
-            if (obj is Duration other)
-            {
-                return Hours == other.Hours &&
-                       Minutes == other.Minutes &&
-                       Seconds == other.Seconds;
-            }
-            return false;
-        }
-
-
-        public override int GetHashCode()
-        {
-           
-            return HashCode.Combine(Hours, Minutes, Seconds);
-        }
-
         #endregion
-
 
         #region Method
-
-
-        public void DisplayDuration()
+        public void NormalizeDuration(int hours, int minutes, int seconds)
         {
-            Console.WriteLine($"Duration: {Hours:D2}:{Minutes:D2}:{Seconds:D2}");
+            minutes += seconds / 60;
+            seconds %= 60;
+
+            hours += minutes / 60;
+            minutes %= 60;
+
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
         }
 
-        #endregion
-
+   
+        public override string ToString()
+        {
+            if (Hours > 0)
+                return $"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}";
+            else
+                return $"Minutes: {Minutes}, Seconds: {Seconds}";
+        }
     
 
- 
+    #endregion
 
 
-      
-   
-    }
+}
 }
--- OOP Assginment 03/Program.cs
+++ OOP Assginment 03/Program.cs
@@ -162,33 +162,60 @@ namespace OOP_Assginment_03
 
             #region 2-Override All System. Object Members (To String, Equals,GetHasCode) 
 
+
+
+
+
+
+            //Duration duration1 = new Duration(1, 120, 360);
+            //Duration duration2 = new Duration(2, 2, 0);
+
+
+            //Console.WriteLine("Duration 1:");
+            //duration1.DisplayDuration();
+
+            //Console.WriteLine("\nDuration 2:");
+            //duration2.DisplayDuration();
+
+
+            //Console.WriteLine(" durations equal? " + duration1.Equals(duration2));
+
+
+            //Console.WriteLine("HashCode for Duration 1: " + duration1.GetHashCode());
+            //Console.WriteLine("HashCode for Duration 2: " + duration2.GetHashCode());
+
+
+            #endregion
+
+
+            #region 3-Define All Required Constructors to Produce this output:
+            //            Duration D1 = new Duration(1, 10, 15);
+            //            D1.ToString();
+            //        Output: Hours: 1, Minutes: 10, Seconds: 15
+            //Duration D1 = new Duration(3600);
+            //            D1.ToString();
+            //        Output: Hours: 1, Minutes: 0, Seconds: 0
+            //Duration D2 = new Duration(7800);
+            //            D2.ToString();
+            //        Output: Hours: 2, Minutes: 10, Seconds: 0
+            //Duration D3 = new Duration(666);
+            //            D3.ToString();
+            //        Output: Minutes: 11, Seconds: 6
+
+
       
-        
+                //Duration D1 = new Duration(1, 10, 15);
+                //Console.WriteLine(D1.ToString()); 
 
-       
-               
-                //Duration duration1 = new Duration(1, 120, 360);
-                //Duration duration2 = new Duration(2, 2, 0);
+                //Duration D2 = new Duration(3600);
+                //Console.WriteLine(D2.ToString()); 
 
-              
-                //Console.WriteLine("Duration 1:");
-                //duration1.DisplayDuration();
+                //Duration D3 = new Duration(7800);
+                //Console.WriteLine(D3.ToString()); 
 
-                //Console.WriteLine("\nDuration 2:");
-                //duration2.DisplayDuration();
-
-             
-                //Console.WriteLine(" durations equal? " + duration1.Equals(duration2));
-
-         
-                //Console.WriteLine("HashCode for Duration 1: " + duration1.GetHashCode());
-                //Console.WriteLine("HashCode for Duration 2: " + duration2.GetHashCode());
-        
-
-    #endregion
-
-
-    #region
+                //Duration D4 = new Duration(666);
+                //Console.WriteLine(D4.ToString()); 
+      
 
 
     #endregion
