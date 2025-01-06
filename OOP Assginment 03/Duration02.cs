5fbd6e6a7f2cf81479ae203bb73565c477f9fe0f
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-06 03:08:29 +0200

    Part 02 Q04

---
+++ OOP Assginment 03/Duration02.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assginment_03
+{
+    internal class Duration02
+    {
+
+        #region Properties
+        public int Hours { get; set; }
+        public int Minutes { get; set; }
+        public int Seconds { get; set; }
+
+        #endregion
+
+        #region Constructors
+        public Duration02(int hours, int minutes, int seconds)
+        {
+            NormalizeDuration(hours, minutes, seconds);
+        }
+
+        public Duration02(int totalSeconds)
+        {
+            NormalizeDuration(0, 0, totalSeconds);
+        }
+
+      
+        public Duration02()
+        {
+            Hours = 0;
+            Minutes = 0;
+            Seconds = 0;
+        }
+
+    
+        private void NormalizeDuration(int hours, int minutes, int seconds)
+        {
+            minutes += seconds / 60;
+            seconds %= 60;
+
+            hours += minutes / 60;
+            minutes %= 60;
+
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+ 
+        public static Duration02 operator +(Duration02 d1, Duration02 d2)
+        {
+            return new Duration02(d1.Hours + d2.Hours, d1.Minutes + d2.Minutes, d1.Seconds + d2.Seconds);
+        }
+
+        public static Duration02 operator +(Duration02 d1, int seconds)
+        {
+            return new Duration02(d1.Hours, d1.Minutes, d1.Seconds + seconds);
+        }
+
+        public static Duration02 operator +(int seconds, Duration02 d2)
+        {
+            return new Duration02(d2.Hours, d2.Minutes, d2.Seconds + seconds);
+        }
+
+
+        public static Duration02 operator -(Duration02 d1, Duration02 d2)
+        {
+            int totalSeconds1 = d1.Hours * 3600 + d1.Minutes * 60 + d1.Seconds;
+            int totalSeconds2 = d2.Hours * 3600 + d2.Minutes * 60 + d2.Seconds;
+            return new Duration02(Math.Max(0, totalSeconds1 - totalSeconds2));
+        }
+        public static Duration02 operator ++(Duration02 d)
+        {
+            return new Duration02(d.Hours, d.Minutes + 1, d.Seconds);
+        }
+
+        public static Duration02 operator --(Duration02 d)
+        {
+            return new Duration02(d.Hours, d.Minutes - 1, d.Seconds);
+        }
+
+        
+        public static bool operator >(Duration02 d1, Duration02 d2)
+        {
+            return d1.TotalSeconds() > d2.TotalSeconds();
+        }
+
+        public static bool operator <(Duration02 d1, Duration02 d2)
+        {
+            return d1.TotalSeconds() < d2.TotalSeconds();
+        }
+
+        public static bool operator >=(Duration02 d1, Duration02 d2)
+        {
+            return d1.TotalSeconds() >= d2.TotalSeconds();
+        }
+
+        public static bool operator <=(Duration02 d1, Duration02 d2)
+        {
+            return d1.TotalSeconds() <= d2.TotalSeconds();
+        }
+
+        
+        public static explicit operator DateTime(Duration02 d)
+        {
+            return new DateTime(1, 1, 1, d.Hours, d.Minutes, d.Seconds);
+        }
+
+       
+        public static implicit operator bool(Duration02 d)
+        {
+            return d.TotalSeconds() > 0;
+        }
+
+        private int TotalSeconds()
+        {
+            return Hours * 3600 + Minutes * 60 + Seconds;
+        }
+        #endregion
+
+        #region Method
+
+        public override string ToString()
+        {
+            if (Hours > 0)
+                return $"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}";
+            else
+                return $"Minutes: {Minutes}, Seconds: {Seconds}";
+        }
+
+        #endregion
+    }
+}
--- OOP Assginment 03/Program.cs
+++ OOP Assginment 03/Program.cs
@@ -6,6 +6,7 @@ namespace OOP_Assginment_03
     {
         static void Main(string[] args)
         {
+            #region Part 01
             #region 1-Write a class named Calculator that contains a method named Add. Overload the Add method to:
             //•	Accept two integers and return their sum.
             //•	Accept three integers and return their sum.
@@ -135,6 +136,8 @@ namespace OOP_Assginment_03
 
             #endregion
 
+            #endregion
+
 
             #region Part 02
 
@@ -203,27 +206,79 @@ namespace OOP_Assginment_03
             //        Output: Minutes: 11, Seconds: 6
 
 
-      
-                //Duration D1 = new Duration(1, 10, 15);
-                //Console.WriteLine(D1.ToString()); 
 
-                //Duration D2 = new Duration(3600);
-                //Console.WriteLine(D2.ToString()); 
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1.ToString()); 
 
-                //Duration D3 = new Duration(7800);
-                //Console.WriteLine(D3.ToString()); 
+            //Duration D2 = new Duration(3600);
+            //Console.WriteLine(D2.ToString()); 
 
-                //Duration D4 = new Duration(666);
-                //Console.WriteLine(D4.ToString()); 
-      
+            //Duration D3 = new Duration(7800);
+            //Console.WriteLine(D3.ToString()); 
 
-    #endregion
+            //Duration D4 = new Duration(666);
+            //Console.WriteLine(D4.ToString()); 
 
-    #region
 
-    #endregion
+            #endregion
 
-    #endregion
-}
+            #region  4 - Implement All required Operators overloading to enable this Code:
+            //•	D3 = D1 + D2
+            //•	D3 = D1 + 7800
+            //•	D3 = 666 + D3
+            //•	D3 = ++D1(Increase One Minute)
+            //•	D3 = --D2(Decrease One Minute)
+            //•	D1 = D1 - D2
+            //•	If(D1 > D2)
+            //•	If(D1 <= D2)
+            //•	If(D1)
+            //•	DateTime Obj = (DateTime)D1
+
+
+
+
+
+            //Duration02 D1 = new Duration02(1, 10, 15);
+            //Duration02 D2 = new Duration02(7800);
+            //Duration02 D3;
+
+
+            //D3 = D1 + D2;
+            //Console.WriteLine("D3 = D1 + D2: " + D3);
+
+            //D3 = D1 + 7800;
+            //Console.WriteLine("D3 = D1 + 7800: " + D3);
+
+            //D3 = 666 + D3;
+            //Console.WriteLine("D3 = 666 + D3: " + D3);
+
+            //D3 = ++D1;
+            //Console.WriteLine("D3 = ++D1: " + D3);
+
+            //D3 = --D2;
+            //Console.WriteLine("D3 = --D2: " + D3);
+
+            //D1 = D1 - D2;
+            //Console.WriteLine("D1 = D1 - D2: " + D1);
+
+            //if (D1 > D2)
+            //    Console.WriteLine("D1 is greater than D2");
+
+            //if (D1 <= D2)
+            //    Console.WriteLine("D1 is less than or equal to D2");
+
+            //if (D1)
+            //    Console.WriteLine("D1 is non-zero");
+
+            //DateTime obj = (DateTime)D1;
+            //Console.WriteLine("DateTime equivalent of D1: " + obj.ToString("HH:mm:ss"));
+
+
+
+            #endregion
+            #endregion
+
+
+        }
     }
 }
