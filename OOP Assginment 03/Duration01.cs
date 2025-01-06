b83bb749ca8d797bdcab1fcb0235b7c1a0d48c2c
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-06 03:12:29 +0200

    Part 02 Q 02

---
+++ OOP Assginment 03/Duration01.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assginment_03
+{
+    internal class Duration01
+    {
+  
+
+            #region Properties
+            public int Hours { get; set; }
+            public int Minutes { get; set; }
+            public int Seconds { get; set; }
+
+#endregion
+
+            #region Constructor
+            public Duration01(int hours, int minutes, int seconds)
+            {
+       
+                if (seconds >= 60)
+                {
+                    minutes += seconds / 60;
+                    seconds %= 60;
+                }
+
+                if (minutes >= 60)
+                {
+                    hours += minutes / 60;
+                    minutes %= 60;
+                }
+
+                Hours = hours;
+                Minutes = minutes;
+                Seconds = seconds;
+            }
+
+
+            public Duration01()
+            {
+                Hours = 0;
+                Minutes = 0;
+                Seconds = 0;
+            }
+
+      
+            public override string ToString()
+            {
+                return $"{Hours:D2}:{Minutes:D2}:{Seconds:D2}";
+            }
+
+    
+            public override bool Equals(object obj)
+            {
+                if (obj is Duration01 other)
+                {
+                    return Hours == other.Hours &&
+                           Minutes == other.Minutes &&
+                           Seconds == other.Seconds;
+                }
+                return false;
+            }
+
+        
+            public override int GetHashCode()
+            {
+           
+                return HashCode.Combine(Hours, Minutes, Seconds);
+            }
+
+        #endregion
+
+            #region Method
+            public void DisplayDuration()
+            {
+                Console.WriteLine($"Duration: {this}");
+            }
+
+            #endregion
+
+
+
+        }
+    }
