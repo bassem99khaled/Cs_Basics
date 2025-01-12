9839a5cb2b13b8baf7d80ec50671a3627f6a3d3b
es-Bassem.Ragab2023@alexu.edu.eg <es-Bassem.Ragab2023@alexu.edu.eg>
2025-01-13 00:44:41 +0200

    Project 01

---
+++ OOP Assignment 05/Program.cs
@@ -0,0 +1,105 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Drawing;
+using OOP_Assignment_05.Project_01;
+namespace OOP_Assignment_05
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1-Define 3D Point Class and the basic Constructors (use chaining in constructors).
+            //     2 - Override the ToString Function to produce this output:
+            //     Point3D P = new Point3D(10, 10, 10);
+            //     Console.WriteLine(P.ToString());
+            //     Output: “Point Coordinates: (10, 10, 10)”.
+            //   3 - Read from the User the Coordinates for 2 points P1, P2(Check the input using try Pares, Parse, Convert).
+
+            //   4 - Try to use ==
+            //   If(P1 == P2)   Does it work properly? 
+            //   5 - Define an array of points and sort this array based on X &Y coordinates.
+            //   6 - Implement ICloneable interface to be able to clone the object.
+            //   To implement more than one interface.
+            //   class Point3D : IComparable, ICloneable
+
+
+
+
+
+            //Point3D P = new Point3D(10, 10, 10);
+            //Console.WriteLine(P.ToString());
+
+
+            //Point3D P1 = ReadPointFromUser("P1");
+            //Point3D P2 = ReadPointFromUser("P2");
+
+
+            //if (P1 == P2)
+            //    Console.WriteLine("P1 and P2 are equal.");
+            //else
+            //    Console.WriteLine("P1 and P2 are not equal.");
+
+
+
+            //Point3D[] pointsArray = {
+            //new Point3D(3, 5, 1),
+            //new Point3D(1, 2, 3),
+            //new Point3D(3, 2, 4),
+            //new Point3D(1, 2, 2)
+            //    };
+
+            //Array.Sort(pointsArray);
+
+            //Console.WriteLine("\nSorted Points:");
+            //foreach (var point in pointsArray)
+            //{
+            //    Console.WriteLine(point);
+            //}
+
+
+
+
+            //Point3D clonedPoint = (Point3D)P1.Clone();
+            //Console.WriteLine($"\nCloned Point: {clonedPoint}");
+            // }
+
+
+           
+            //static Point3D ReadPointFromUser(string pointName)
+            //{
+            //    int x, y, z;
+
+            //    Console.WriteLine($"\nEnter coordinates for {pointName}:");
+
+            //    x = ReadCoordinate("X");
+            //    y = ReadCoordinate("Y");
+            //    z = ReadCoordinate("Z");
+
+            //    return new Point3D(x, y, z);
+            //}
+
+            
+
+
+            //static int ReadCoordinate(string coordinateName)
+            //{
+            //    int value;
+            //    while (true)
+            //    {
+            //        Console.Write($"Enter {coordinateName}: ");
+            //        string input = Console.ReadLine();
+
+            //        if (int.TryParse(input, out value))
+            //            break;
+            //        else
+            //            Console.WriteLine("Invalid input. Please enter an integer.");
+            //    }
+            //    return value;
+           
+
+
+
+
+        #endregion
+    }
+}
+}
---
+++ OOP Assignment 05/Project 01/Point3D.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_Assignment_05.Project_01
+{
+    internal class Point3D : IComparable, ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Z { get; set; }
+
+
+        public Point3D() : this(0, 0, 0)
+        {
+
+        }
+
+
+        public Point3D(int value) : this(value, value, value)
+        {
+
+
+        }
+
+
+        public Point3D(int x, int y, int z)
+        {
+            X = x;
+            Y = y;
+            Z = z;
+        }
+
+
+        public override string ToString()
+        {
+            return $"Point Coordinates: ({X}, {Y}, {Z})";
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Point3D other)
+            {
+                return X == other.X && Y == other.Y && Z == other.Z;
+            }
+            return false;
+        }
+
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(X, Y, Z);
+        }
+
+
+        public static bool operator ==(Point3D p1, Point3D p2)
+        {
+            if (ReferenceEquals(p1, p2)) return true;
+            if (ReferenceEquals(p1, null) || ReferenceEquals(p2, null)) return false;
+            return p1.Equals(p2);
+        }
+
+
+        public static bool operator !=(Point3D p1, Point3D p2)
+        {
+            return !(p1 == p2);
+        }
+
+
+        public int CompareTo(Point3D other)
+        {
+            if (X != other.X)
+                return X.CompareTo(other.X);
+            if (Y != other.Y)
+                return Y.CompareTo(other.Y);
+            return Z.CompareTo(other.Z);
+        }
+
+
+        public object Clone()
+        {
+            return new Point3D(X, Y, Z);
+        }
+
+        int IComparable.CompareTo(object? obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
